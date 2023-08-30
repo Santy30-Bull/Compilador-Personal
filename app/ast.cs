3d3418a8302a4939bcd3746f64dc4553a73cdc4d
santiago.toro62@eia.edu.co <santiago.toro62@eia.edu.co>
2023-08-30 14:45:35 -0500

    Agrego codico ast

---
+++ app/ast.cs
@@ -0,0 +1,76 @@
+using System.Collections.Generic;
+
+    public abstract class ASTNode
+    {
+        public abstract string TokenLiteral();
+        public abstract override string ToString();
+    }
+
+    public class Statement : ASTNode
+    {
+        private Token token;
+
+        public Statement(Token token)
+        {
+            this.token = token;
+        }
+
+        public override string TokenLiteral()
+        {
+            return token.Literal;
+        }
+
+        public override string ToString()
+        {
+            return TokenLiteral();
+        }
+    }
+
+    public class Expression : ASTNode
+    {
+        private Token token;
+
+        public Expression(Token token)
+        {
+            this.token = token;
+        }
+
+        public override string TokenLiteral()
+        {
+            return token.Literal;
+        }
+
+        public override string ToString()
+        {
+            return TokenLiteral();
+        }
+    }
+
+    public class Program : ASTNode
+    {
+        private List<Statement> statements;
+
+        public Program(List<Statement> statements)
+        {
+            this.statements = statements;
+        }
+
+        public override string TokenLiteral()
+        {
+            if (statements.Count > 0)
+            {
+                return statements[0].TokenLiteral();
+            }
+            return "";
+        }
+
+        public override string ToString()
+        {
+            List<string> outList = new List<string>();
+            foreach (Statement statement in statements)
+            {
+                outList.Add(statement.ToString());
+            }
+            return string.Join("", outList);
+        }
+    }
