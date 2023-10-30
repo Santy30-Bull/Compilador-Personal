465d4939a2394be60f060756f8a1f9f0a302adbb
Santiago A Toro B <139780353+Santy30-Bull@users.noreply.github.com>
2023-10-30 12:20:25 -0500

    Ultimos cambios al MainClass.cs
    Se agrega la capacidad de declarar funciones, y de evaluar las variables. Pero esta en fase muy verde

--- Compilador con logica (1)/MainClass.cs
+++ Compilador con logica (1)/MainClass.cs
@@ -1,44 +1,171 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 class MainClass
 {
+    static Dictionary<string, Func<double, double>> funciones = new Dictionary<string, Func<double, double>>();
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Bienvenido al analizador léxico");
+        Console.WriteLine("Bienvenido al evaluador de funciones en C#.");
 
         while (true)
         {
             Console.Write(">> ");
-            string input = Console.ReadLine() ?? ""; // Si Console.ReadLine() devuelve null, asigna una cadena vacía
+            string entrada = Console.ReadLine();
 
-            if (string.IsNullOrWhiteSpace(input))
+            if (entrada == "salir")
                 break;
-
-            Lexer lexer = new Lexer(input);
-            List<Token> tokens = lexer.Tokenize(); // Tokenize the input
-
-            Parser parser = new Parser(tokens);
-
-            try
+            else if (entrada.Contains("="))
             {
-                double result = parser.Parse(); // Parse the tokens and get the result
-                if(result == 1){
-                    Console.WriteLine("Resultado: " + true);
+                string[] partes = entrada.Split('=');
+                if (partes.Length == 2)
+                {
+                    string nombre = partes[0].Trim();
+                    string expresion = partes[1].Trim();
+                    try
+                    {
+                        funciones[nombre] = x => EvaluarExpresion(expresion, x);
+                        Console.WriteLine($"Función '{nombre}' asignada.");
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine($"Error al asignar la función: {e.Message}");
+                    }
                 }
-                else if(result == 0){
-                    Console.WriteLine("Resultado: " + false);
-                }else{
-                    Console.WriteLine("Resultado: " + result);
+                else
+                {
+                    Console.WriteLine("Entrada no válida. Use 'nombre = expresión' para asignar una función.");
                 }
-                    
             }
-            catch (Exception e)
+            else
             {
-                Console.WriteLine("Error: " + e.Message);
+                try
+                {
+                    double resultado = EvaluarExpresionConArgumento(entrada);
+                    Console.WriteLine($"Resultado: {resultado}");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Error al evaluar la función: {e.Message}");
+                }
             }
+        }
+
+        Console.WriteLine("Saliendo del programa.");
+    }
+
+static double EvaluarExpresion(string expresion, double x)
+{
+    try
+    {
+        var tokens = expresion.Split(' ');
+
+        double resultado = 0;
+        double operando = 0;
+        string operador = "+";
+
+        foreach (var token in tokens)
+        {
+            if (double.TryParse(token, out double valor))
+            {
+                operando = valor;
+
+                if (operador == "+")
+                    resultado += operando;
+                else if (operador == "-")
+                    resultado -= operando;
+                else if (operador == "*")
+                    resultado *= operando;
+                else if (operador == "/")
+                {
+                    if (operando != 0)
+                        resultado /= operando;
+                    else
+                        throw new DivideByZeroException("División por cero.");
+                }
+            }
+            else if (token == "+")
+                operador = "+";
+            else if (token == "-")
+                operador = "-";
+            else if (token == "*")
+                operador = "*";
+            else if (token == "/")
+                operador = "/";
+            else if (token == "x")
+            {
+                operando = x;
+
+                if (operador == "+")
+                    resultado += operando;
+                else if (operador == "-")
+                    resultado -= operando;
+                else if (operador == "*")
+                    resultado *= operando;
+                else if (operador == "/")
+                {
+                    if (operando != 0)
+                        resultado /= operando;
+                    else
+                        throw new DivideByZeroException("División por cero.");
+                }
+            }
+        }
+        return resultado;
+    }
+    catch (Exception e)
+    {
+        throw new Exception($"Error al evaluar la expresión: {e.Message}");
+    }
+}
+
+ 
+static double EvaluarTokens(string[] tokens, double x)
+    {
+        double resultado = 0;
+        string operador = "+";
+        foreach (var token in tokens)
+        {
+            if (double.TryParse(token, out double valor))
+            {
+                if (operador == "+")
+                    resultado += valor;
+                else if (operador == "-")
+                    resultado -= valor;
+            }
+            else if (token == "+")
+            {
+                operador = "+";
+            }
+            else if (token == "-")
+            {
+                operador = "-";
+            }
+            else if (token == "x")
+            {
+                resultado += x;
+            }
+        }
+        return resultado;
+    }
+
+    static double EvaluarExpresionConArgumento(string entrada)
+    {
+        var partes = entrada.Split('(', ')');
+        if (partes.Length != 3)
+            throw new ArgumentException("Formato de entrada no válido.");
+
+        string nombre = partes[0].Trim();
+        double argumento = double.Parse(partes[1]);
+
+        if (funciones.ContainsKey(nombre))
+        {
+            return funciones[nombre](argumento);
+        }
+        else
+        {
+            throw new ArgumentException($"La función '{nombre}' no está definida.");
         }
     }
 }
